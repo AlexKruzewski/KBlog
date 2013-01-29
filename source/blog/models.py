56156ff8f4016ec2bb455d48f659fd4fdac0a42b
Alex Kruzewski <a.t.kruzewski@gmail.com>
2013-01-30 00:01:22 +0100

    setup of poll model and inclusion of blog app in project settings

--- source/blog/models.py
+++ source/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('Date Published')
+    content = models.TextField()
+    slug = models.SlugField()
+    link = models.URLField(blank=True)
+
+    def __unicode__(self):
+        return self.title
