464f7e79f9354666e1021d1ed901ae717a3faa51
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2013-01-29 23:48:18 +0100

    More changes to the settings file

--- source/BlogProject/settings.py
+++ source/BlogProject/settings.py
@@ -1,8 +1,8 @@
-# Django settings for BlogProject project.
+import os
 
+SITE_ROOT = os.path.dirname(__file__)
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-
 ADMINS = (
     # ('',''),
 )
@@ -28,7 +28,7 @@ TIME_ZONE = 'Europe/London'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-uk'
+LANGUAGE_CODE = 'en-gb'
 
 SITE_ID = 1
 
@@ -45,18 +45,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(SITE_ROOT, '../../media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(SITE_ROOT, '../../static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
