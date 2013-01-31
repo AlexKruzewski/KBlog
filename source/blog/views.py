0fa0a2e54e40c48ac63abdcaf4b5efb2fa31c72b
Alex Kruzewski <a.t.kruzewski@gmail.com>
2013-01-31 16:35:56 +0100

    added new views and improved upon index view. Added base temaplte and post template, modified index to use base

--- source/blog/views.py
+++ source/blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 def index(request):
-    all_blog_posts = Post.objects.all()
-    return render_to_response('blog/index.html', {'all_blog_posts': all_blog_posts})
+    published_blog_posts = Post.objects.filter(published = True)
+    return render(request, 'blog/index.html', {'published_blog_posts': published_blog_posts})
+
+def post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'blog/post.html', {'post': post})
