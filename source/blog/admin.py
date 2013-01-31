4e4494f70d77bf82f1d6b1183f8b19bfe1818346
Alex Kruzewski <a.t.kruzewski@gmail.com>
2013-01-31 15:55:56 +0100

    added new fields to poll model and editted the admin interface to reflect these changes

--- source/blog/admin.py
+++ source/blog/admin.py
@@ -1,4 +1,12 @@
 from blog.models import Post
 from django.contrib import admin
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    list_filter = ['published','pub_date']
+    search_fields = ['title', 'description', 'content']
+    date_heirarchy = 'pub_date'
+    save_on_top = True
+    prepopulated_field = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
--- source/blog/models.py
+++ source/blog/models.py
@@ -3,9 +3,16 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField('Date Published')
+    description = models.CharField(max_length=255)
     content = models.TextField()
-    slug = models.SlugField()
-    link = models.URLField(blank=True)
+    slug = models.SlugField(unique=True, max_length=255)
+    published = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['pub_date']
 
     def __unicode__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog.views.post', args=[self.slug])
