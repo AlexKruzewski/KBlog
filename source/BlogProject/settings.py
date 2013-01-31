8dd30545e855e7bb2f367c024fb21c807e692c13
Alex Kruzewski <a.t.kruzewski@gmail.com>
2013-01-31 01:02:11 +0100

    Started work on the blog views, with a test index view

--- source/BlogProject/settings.py
+++ source/BlogProject/settings.py
@@ -104,8 +104,7 @@ WSGI_APPLICATION = 'BlogProject.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    "/home/vagrant/Projects/KBlog/templates"
 )
 
 INSTALLED_APPS = (
--- source/blog/views.py
+++ source/blog/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from blog.models import Post
+
+def index(request):
+    all_blog_posts = Post.objects.all()
+    return render_to_response('blog/index.html', {'all_blog_posts': all_blog_posts})
