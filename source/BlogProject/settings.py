d3a94f92ea3b289d0353f7e83518bea7e7f21227
Alex Kruzewski <a.t.kruzewski@gmail.com>
2013-02-23 19:39:40 +0100

    style and layout changes

--- source/BlogProject/settings.py
+++ source/BlogProject/settings.py
@@ -45,7 +45,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(SITE_ROOT, '../../media/')
+MEDIA_ROOT = os.path.join(SITE_ROOT, '/media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -56,7 +56,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(SITE_ROOT, '../../static/')
+STATIC_ROOT = os.path.join(SITE_ROOT, '../../../static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -67,6 +67,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT, '../../static'),
 )
 
 # List of finder classes that know how to find static files in
