f113a19a43830ab3dd44133fe43f83e6c275166a
Alex Kruzewski <a.t.kruzewski@gmail.com>
2013-03-16 12:38:09 +0100

    Added media root to urls

--- source/BlogProject/urls.py
+++ source/BlogProject/urls.py
@@ -5,6 +5,10 @@ from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root' : settings.MEDIA_ROOT,
+    }),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'blog.views.index'),
     url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
